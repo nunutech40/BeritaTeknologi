3c8f2eb1864dd03c6556e7282dbc82109f1b5bb5
nunu <nunutech4.0@gmail.com>
2020-11-12 13:52:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './pages';
 
 export default function App() {
+  
+  useEffect(() => {
+    console.log('test debug')
+  }, )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor="blue" />
+      <Home/>
     </View>
   );
 }
@@ -14,8 +20,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/pages/Home/index.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import { FlatList, ScrollView, Dimensions, StyleSheet, Text, View, Image, Linking } from 'react-native'
+
+const devwidth = Dimensions.get('window').width;
+
+
+const HeaderBerita =() => {
+    return(
+        <View>
+            <Text style={{fontSize: 30, fontWeight: 'bold', textAlign: 'center', paddingVertical: 20}}>Berita Teknologi Terupdate</Text>
+            <View style={{height: 15, backgroundColor: '#efefefef'}}></View>
+        </View>
+    )
+}
+const CellBerita = ({ item }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", paddingHorizontal: 30 }}>
+      <Image
+        style={{
+          height: devwidth / 2,
+          width: devwidth,
+          marginTop: 20,
+          marginBottom: 15,
+        }}
+        source={{ uri: item.poster }}
+      />
+      <View style={{ width: "110%" }}>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{item.judul}</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Text
+            style={{ fontSize: 18, color: "blue" }}
+            onPress={() => Linking.openURL(item.link)}
+          >
+            Lihat Berita
+          </Text>
+          <Text style={{ textAlign: "right" }}>{item.waktu}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+const Home = () => {
+  var TempData = [];
+  const [dataBerita, setDataBerita] = useState([{
+      judul:'',
+      link: '',
+      poster: '',
+      tipe: '',
+      waktu: ''
+  }])
+  useEffect(() => {
+    //call API method GET
+    fetch("https://www.news.developeridn.com/teknologi")
+      .then((response) => response.json())
+      .then(json => {
+          setDataBerita(json.data)
+          console.log(dataBerita)
+      });
+
+    //call api method pos
+    
+    
+    // console.log('data object', dataBerita)
+    // console.log('data strigfy',JSON.stringify(dataBerita))
+    // fetch("https://reqres.in/api/users",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(dataBerita),
+    //   })
+    //   .then(response => response.json())
+    //   .then(json => {
+    //       console.log('post response databerita: ', dataBerita)
+    //   })
+  }, []);
+  return (
+    <View style={{ flex: 1, marginTop: 20 }}>
+      <FlatList
+        style={{ backgroundColor: "white" }}
+        data={dataBerita}
+        keyExtractor={(item, index) => item.index}
+        renderItem={({ item }) => <CellBerita item={item} />}
+        ListHeaderComponent={HeaderBerita}
+      />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({})
